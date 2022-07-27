19893cc86f8c05c6960bb771f69df68c6b066a45
Joe Posthauer <posthauerjoe@gmail.com>
2022-07-26 17:24:26 -0700

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import hash from "./hash";
+
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "67a7a559635442d7a49f69ba3e0f6b8e";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
 
 function App() {
-    return (
-        <div></div>
-    )
+
+  const [token, setToken] = useState(null);
+
+
+
+  useEffect(() => {
+    var mToken = hash.access_token;
+    if (mToken) {
+      setToken(mToken);
+    }
+  })
+
+
+
+  return (
+    <div>
+      {!token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            "%20"
+          )}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <div className="header">
+            Hello
+        </div>
+    )
+}
+
+export default Header;
