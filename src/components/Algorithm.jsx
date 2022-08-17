ca760ff18caf8b51db6c12a9c107bd031c9cc0c9
Gillian Feder <gillianfeder@Gillians-MacBook-Pro-2.local>
2022-08-17 17:48:30 -0400

    Organization and Algo advancement

--- src/components/Algorithm.jsx
+++ src/components/Algorithm.jsx
@@ -28,6 +28,7 @@ function Algorithm (props) {
         xhr.onload = callback;
     }
 
+    //Component 1: Where the GD fall on their top artists list
     //Determines where the grateful dead fall in their top artists list
     function gdRanking(){
         topArtists.forEach((artist,index) => {
@@ -39,9 +40,8 @@ function Algorithm (props) {
         })
     }
 
-
     //Returns proper percentage (with a total weight of 40) of GD within top artists
-    function gdRankingPercentage(ranking) {
+    function gdRankingPercentage(ranking) {   //ranking param = gdIndex
         //if dead not w/in top 20 artists, they get no points
         if (ranking == -1) {
             return 0;
@@ -50,15 +50,55 @@ function Algorithm (props) {
         return (0.4-(ranking*2));
     }
 
+
+    //Component 2: How many of their top tracks are by the GD
     function gdSongsPercentage() {
         //keep track of summated song inverses
         var fracTotal = 0;
 
         topTracks.forEach((song, index) => {
-            console.log(song.name);
+            if (song.artist[0].name == "Grateful Dead") {
+                fracTotal+=(1/index);
+                console.log(song.name); //to check if correct
+            }
         });
+        return fracTotal;
     }
 
+
+    //Component 3: How many of their top artists are related to the GD
+    function topRelatedArtists(){
+        var fracTotal = 0;
+        topArtists.forEach((artist,index) => {
+            if (artist in relatedArtists) { //does this work in JS; if not, need nested
+                fracTotal += (1/index);
+            }
+
+            return fracTotal;
+         });
+    }
+
+    
+    //Component 4: How many of their top songs are by related artists
+    function songsByRelated() {
+        //keep track of summated song inverses
+        var fracTotal = 0;
+
+        topTracks.forEach((song, index) => {
+            if (song.artist[0].name in relatedArtists) {
+                fracTotal+=(1/index);
+                console.log(song.artist[0].name); //to check if correct
+            }
+        });
+        return fracTotal;
+    }
+
+
+    //Total Up their score
+    function addComponents() {
+        //figure out multiplier for each component and add them all up
+    }
+    
     //console.log(gdRankingPercentage(gdRanking()));
     gdSongsPercentage();
     gdRanking();
